76c7a067eff01fca3f39e078e4d854e0052f2dd2
Crowdbotics <team@crowdbotics.com>
2023-03-15 09:12:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "creekline-farms-39397",
+  url: "https://creekline-farms-39397.botics.co",
+  api: "https://creekline-farms-39397.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
